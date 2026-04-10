859581167884fa058112e146f1ffdab4b5502780
llewell <llwll@MacBoo.local>
2026-04-11 00:16:27 +0500

    init project

---
+++ src/main/java/org/aitu/vulnerabilitiesmvp/VulnerabilitiesMvpApplication.java
@@ -0,0 +1,13 @@
+package org.aitu.vulnerabilitiesmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnerabilitiesMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnerabilitiesMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aitu/vulnerabilitiesmvp/VulnerabilitiesMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aitu.vulnerabilitiesmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnerabilitiesMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
